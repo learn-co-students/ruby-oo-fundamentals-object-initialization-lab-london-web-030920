ee9e5f574a2589255578146a658d993acedca22a
minamalaj <mina.malaj@hotmail.com>
2020-03-10 13:39:37 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+class Dog 
+    def initialize(name, breed = "Mutt")
+        @name = name 
+        @breed = breed
+    end 
+   def breed=(breed)
+    @breed = bread 
+   end 
+   def name=(name)
+    @name = name 
+   end 
+   def breed
+    @breed 
+   end 
+   def name
+    @name
+   end 
+end 
